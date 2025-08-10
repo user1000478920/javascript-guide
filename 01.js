64e124cc54dd2b512ba26bb6b876981330dea6ec
user1000478920 <soham.roat2009@gmail.com>
2025-08-10 17:54:57 +0530

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,2 +1,10 @@
 console.log("Hello World")
-// comment
+// comments are used to give a bit of 
+// context to the person reading the code
+// types of comments: 
+// Single line comments are denoted by `//...`
+/*
+multi
+line
+comments are denoted by `/* ... */`
+*/
