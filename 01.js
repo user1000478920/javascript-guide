ffa3754d85192be6336e2490b51b5490d9eee169
user1000478920 <soham.roat2009@gmail.com>
2025-08-11 15:59:32 +0530

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,10 +1,14 @@
 console.log("Hello World")
 // comments are used to give a bit of 
 // context to the person reading the code
+// The code written inside comments isn't executed.
 // types of comments: 
 // Single line comments are denoted by `//...`
-/*
-multi
-line
-comments are denoted by `/* ... */`
+/* multi line comments 
+are denoted by `/* ... */`
+*/
+/* Example:
+console.log() is a function that displays the text
+(in a console) between the brackets. It is commonly
+used to display output of code in javascripts.
 */
